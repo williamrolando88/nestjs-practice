61d53cd0c8e1f06d7091bcde17e9923fb410e068
William Morales <william.rolando88@hotmail.com>
2025-12-21 08:07:33 -0500

    Add validation decorators to CreateProductDto fields

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,39 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsIn,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  stock: number;
+
+  @IsArray()
+  @IsString({ each: true })
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+}
