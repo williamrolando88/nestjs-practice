feed15ab942140e1f9ffb7fa1d1b79a51f619bcd
William Morales <william.rolando88@hotmail.com>
2025-12-21 08:01:58 -0500

    Set global prefix and validation pipes in the application

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import 'reflect-metadata';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
